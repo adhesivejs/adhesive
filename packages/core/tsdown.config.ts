15b72b2520503aedeba6b3c67119fbbde818e176
Daniel Waltz <danielbwaltz@gmail.com>
2025-07-27 15:42:39 -0400

    chore: support publishing to jsr

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: "src/index.ts",
   platform: "neutral",
-  exports: true,
+  exports: {
+    customExports: (exports) => ({
+      ...exports,
+      ".": {
+        jsr: "./src/index.ts",
+        types: "./dist/index.d.ts",
+        import: "./dist/index.js",
+      },
+    }),
+  },
 });
--- packages/vue/tsdown.config.ts
+++ packages/vue/tsdown.config.ts
@@ -4,7 +4,16 @@ import Vue from "unplugin-vue/rolldown";
 export default defineConfig({
   entry: "src/index.ts",
   platform: "neutral",
-  exports: true,
+  exports: {
+    customExports: (exports) => ({
+      ...exports,
+      ".": {
+        jsr: "./src/index.ts",
+        types: "./dist/index.d.ts",
+        import: "./dist/index.js",
+      },
+    }),
+  },
   plugins: [Vue({ isProduction: true })],
   dts: { vue: true },
 });
