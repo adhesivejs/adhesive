d2caaf599467f155e4f9a98308049a3fd865aa3b
Daniel Waltz <danielbwaltz@gmail.com>
2025-07-27 11:36:49 -0400

    chore: lint cleanup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
 import { danielwaltz } from "@danielwaltz/eslint-config";
+import { defineConfig } from "eslint/config";
 
-export default danielwaltz();
+const globalIgnoresConfig = defineConfig({
+  ignores: ["pnpm-workspace.yaml"],
+});
+
+export default danielwaltz({ pnpm: true }).append(globalIgnoresConfig);
